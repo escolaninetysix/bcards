699709bb5e918132d71023ca81cf39e93221d318
Mike Escola <mescola@borisfx.com>
2017-01-03 07:18:49 -0500

    A solution to the cards puzzle

--- bcards/main.cpp
+++ bcards/main.cpp
@@ -60,87 +60,135 @@ bool testcards(struct cardset thecardset)
 }
 int main ()
 {
-    
     struct cardset thiscardset;
-    for (int itr1 = 0; itr1 < 36; ++itr1)
+    unsigned int cardinslot[9] = { 0 };
+    bool cardspushed[9] = { 0 }; // Initial state all cards are pushed
+    unsigned int depthpushed = 0;
+    unsigned int positr[9];
+    for (positr[0]=0; positr[0]<9; ++positr[0]) // 0
     {
-        for (int itr2 = 0; itr2 < 36; ++itr2)
+        cardinslot[0]=positr[0];
+        cardspushed[positr[0]] = true;
+        for (positr[1]=0; positr[1]<9; ++positr[1]) // 1
         {
-            for (int itr3 = 0; itr3 < 36; ++itr3)
+            cardinslot[1]=positr[1];
+            if (cardspushed[positr[1]] == true) { continue; }
+            cardspushed[positr[1]] = true;
+            
+            for (positr[2]=0; positr[2]<9; ++positr[2]) // 2
             {
-                for (int itr4 = 0; itr4 < 36; ++itr4)
+                cardinslot[2]=positr[2];
+                if (cardspushed[positr[2]] == true) { continue; }
+                cardspushed[positr[2]] = true;
+                
+                for (positr[3]=0; positr[3]<9; ++positr[3]) // 3
                 {
-                    for (int itr5 = 0; itr5 < 36; ++itr5)
+                    cardinslot[3]=positr[3];
+                    if (cardspushed[positr[3]] == true) { continue; }
+                    cardspushed[positr[3]] = true;
+                    
+                    for (positr[4]=0; positr[4]<9; ++positr[4]) // 4
                     {
-                        for (int itr6 = 0; itr6 < 36; ++itr6)
+                        cardinslot[4]=positr[4];
+                        if (cardspushed[positr[4]] == true) { continue; }
+                        cardspushed[positr[4]] = true;
+                        
+                        for (positr[5]=0; positr[5]<9; ++positr[5]) // 5
                         {
-                            for (int itr7 = 0; itr7 < 36; ++itr7)
+                            cardinslot[5]=positr[5];
+                            if (cardspushed[positr[5]] == true) { continue; }
+                            cardspushed[positr[5]] = true;
+                            
+                            for (positr[6]=0; positr[6]<9; ++positr[6]) // 6
                             {
-                                for (int itr8 = 0; itr8 < 36; ++itr8)
+                                cardinslot[6]=positr[6];
+                                if (cardspushed[positr[6]] == true) { continue; }
+                                cardspushed[positr[6]] = true;
+                                
+                                for (positr[7]=0; positr[7]<9; ++positr[7]) // 7
                                 {
-                                    for (int itr9 = 0; itr9 < 36; ++itr9)
+                                    cardinslot[7]=positr[7];
+                                    if (cardspushed[positr[7]] == true) { continue; }
+                                    cardspushed[positr[7]] = true;
+                                    
+                                    for (positr[8]=0; positr[8]<9; ++positr[8]) // 8
                                     {
-                                        bool hasrepeat = false;
-                                        bool whichhit[9] = {false,false,false,false,false,false,false,false,false};
+                                        cardinslot[8]=positr[8];
+                                        if (cardspushed[positr[8]] == true) { continue; }
+                                        cardspushed[positr[8]] = true;
                                         
-                                        thiscardset.bcard[0]=itr1%9;
-                                        thiscardset.bface[0]=itr1/9;
-                                        thiscardset.bcard[1]=itr2%9;
-                                        thiscardset.bface[1]=itr2/9;
-                                        thiscardset.bcard[2]=itr3%9;
-                                        thiscardset.bface[2]=itr3/9;
-                                        thiscardset.bcard[3]=itr4%9;
-                                        thiscardset.bface[3]=itr4/9;
-                                        thiscardset.bcard[4]=itr5%9;
-                                        thiscardset.bface[4]=itr5/9;
-                                        thiscardset.bcard[5]=itr6%9;
-                                        thiscardset.bface[5]=itr6/9;
-                                        thiscardset.bcard[6]=itr7%9;
-                                        thiscardset.bface[6]=itr7/9;
-                                        thiscardset.bcard[7]=itr8%9;
-                                        thiscardset.bface[7]=itr8/9;
-                                        thiscardset.bcard[8]=itr9%9;
-                                        thiscardset.bface[8]=itr9/9;
                                         
-                                        for (unsigned int itr = 0; itr < 9; ++itr)
+                                        
+                                        
+                                        
+                                        
+                                        thiscardset.bcard[0]=cardinslot[0];
+                                        thiscardset.bcard[1]=cardinslot[1];
+                                        thiscardset.bcard[2]=cardinslot[2];
+                                        thiscardset.bcard[3]=cardinslot[3];
+                                        thiscardset.bcard[4]=cardinslot[4];
+                                        thiscardset.bcard[5]=cardinslot[5];
+                                        thiscardset.bcard[6]=cardinslot[6];
+                                        thiscardset.bcard[7]=cardinslot[7];
+                                        thiscardset.bcard[8]=cardinslot[8];
+                                        unsigned int faceitr[9];
+                                        for (faceitr[0]=0; faceitr[0]<4; ++faceitr[0])
                                         {
-                                            if (whichhit[ thiscardset.bcard[itr] ]==false)
+                                            for (faceitr[1]=0; faceitr[1]<4; ++faceitr[1])
                                             {
-                                                whichhit[ thiscardset.bcard[itr] ]= true;
-                                            }
-                                            else
-                                            {
-                                                hasrepeat = true;
-                                                continue;
+                                                for (faceitr[2]=0; faceitr[2]<4; ++faceitr[2])
+                                                {
+                                                    for (faceitr[3]=0; faceitr[3]<4; ++faceitr[3])
+                                                    {
+                                                        for (faceitr[4]=0; faceitr[4]<4; ++faceitr[4])
+                                                        {
+                                                            for (faceitr[5]=0; faceitr[5]<4; ++faceitr[5])
+                                                            {
+                                                                for (faceitr[6]=0; faceitr[6]<4; ++faceitr[6])
+                                                                {
+                                                                    for (faceitr[7]=0; faceitr[7]<4; ++faceitr[7])
+                                                                    {
+                                                                        for (faceitr[8]=0; faceitr[8]<4; ++faceitr[8])
+                                                                        {
+                                                                            thiscardset.bface[0]=faceitr[0];
+                                                                            thiscardset.bface[1]=faceitr[1];
+                                                                            thiscardset.bface[2]=faceitr[2];
+                                                                            thiscardset.bface[3]=faceitr[3];
+                                                                            thiscardset.bface[4]=faceitr[4];
+                                                                            thiscardset.bface[5]=faceitr[5];
+                                                                            thiscardset.bface[6]=faceitr[6];
+                                                                            thiscardset.bface[7]=faceitr[7];
+                                                                            thiscardset.bface[8]=faceitr[8];
+                                                                            bool bfail = testcards(thiscardset);
+                                                                            if (!bfail) { cout << "Successful\n"; for (unsigned int itr = 0; itr < 9; ++itr) {cout << cardinslot[itr] << "," << faceitr[itr] <<"\n";} }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
                                             }
                                         }
-                                        if (hasrepeat) { continue; }
                                         
-                                        bool bfail = testcards(thiscardset);
                                         
-                                        if (!bfail)
-                                        {
-                                            cout << "Successful\n";
-                                            cout << itr1 << "\n";
-                                            cout << itr2 << "\n";
-                                            cout << itr3 << "\n";
-                                            cout << itr4 << "\n";
-                                            cout << itr5 << "\n";
-                                            cout << itr6 << "\n";
-                                            cout << itr7 << "\n";
-                                            cout << itr8 << "\n";
-                                            cout << itr9 << "\n";
-                                            
-                                        }
-                                                                                      
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+                                        cardspushed[positr[8]] = false;
+                                    } // 8
+                                    cardspushed[positr[7]] = false;
+                                } // 7
+                                cardspushed[positr[6]] = false;
+                            } // 6
+                            cardspushed[positr[5]] = false;
+                        } // 5
+                        cardspushed[positr[4]] = false;
+                    } // 4
+                    cardspushed[positr[3]] = false;
+                } // 3
+                cardspushed[positr[2]] = false;
+            } // 2
+            cardspushed[positr[1]] = false;
+        } // 1
+        cardspushed[positr[0]] = false;
+    } // 0
     return 0;
 }
